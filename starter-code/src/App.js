225c7a74b015b1656592360076b6816aadd53a79
Saeka Isozumi <isozumisaeka@gmail.com>
2020-02-17 17:57:12 +0100

    done

---
+++ starter-code/src/App.js
@@ -0,0 +1,180 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar is-transparent">
+      <div className="navbar-brand">
+        <a className="navbar-item" href="https://bulma.io">
+          <img
+            src="https://bulma.io/images/bulma-logo.png"
+            alt="Bulma: a modern CSS framework based on Flexbox"
+            width="112"
+            height="28"
+          />
+        </a>
+        <div
+          className="navbar-burger burger"
+          data-target="navbarExampleTransparentExample"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+
+      <div id="navbarExampleTransparentExample" className="navbar-menu">
+        <div className="navbar-start">
+          <a className="navbar-item" href="https://bulma.io/">
+            Home
+          </a>
+          <div className="navbar-item has-dropdown is-hoverable">
+            <a
+              className="navbar-link"
+              href="https://bulma.io/documentation/overview/start/"
+            >
+              Docs
+            </a>
+            <div className="navbar-dropdown is-boxed">
+              <a
+                className="navbar-item"
+                href="https://bulma.io/documentation/overview/start/"
+              >
+                Overview
+              </a>
+              <a
+                className="navbar-item"
+                href="https://bulma.io/documentation/modifiers/syntax/"
+              >
+                Modifiers
+              </a>
+              <a
+                className="navbar-item"
+                href="https://bulma.io/documentation/columns/basics/"
+              >
+                Columns
+              </a>
+              <a
+                className="navbar-item"
+                href="https://bulma.io/documentation/layout/container/"
+              >
+                Layout
+              </a>
+              <a
+                className="navbar-item"
+                href="https://bulma.io/documentation/form/general/"
+              >
+                Form
+              </a>
+              <hr className="navbar-divider" />
+              <a
+                className="navbar-item"
+                href="https://bulma.io/documentation/elements/box/"
+              >
+                Elements
+              </a>
+              <a
+                className="navbar-item is-active"
+                href="https://bulma.io/documentation/components/breadcrumb/"
+              >
+                Components
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="field is-grouped">
+              <p className="control">
+                <a
+                  className="bd-tw-button button"
+                  data-social-network="Twitter"
+                  data-social-action="tweet"
+                  data-social-target="https://bulma.io"
+                  target="_blank"
+                  href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=https://bulma.io&amp;via=jgthms"
+                >
+                  <span className="icon">
+                    <i className="fab fa-twitter"></i>
+                  </span>
+                  <span>Login</span>
+                </a>
+              </p>
+              <p className="control">
+                <a
+                  className="button is-primary"
+                  href="https://github.com/jgthms/bulma/releases/download/0.8.0/bulma-0.8.0.zip"
+                >
+                  <span className="icon">
+                    <i className="fas fa-download"></i>
+                  </span>
+                  <span>Signup</span>
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+const FormField = props => {
+  return (
+    <div className="field">
+      <label className="label">{props.label}</label>
+      <div className="control">
+        <input
+          className="input"
+          type={props.type}
+          placeholder={props.placeHolder}
+        />
+      </div>
+    </div>
+  );
+};
+
+const CoolButton = props => {
+  return (
+    <div>
+      <button className={props.button1Style}>SignUp</button>
+      {/* <button className={props.button2Style}>Button 2</button> */}
+    </div>
+  );
+};
+
+const Signup = () => {
+  return (
+    <div>
+      <Navbar />
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+      <FormField label="email" type="text" placeholder="e.g Alex Smith" />
+      <FormField label="password" type="text" placeholder="e.g Alex Smith" />
+      <CoolButton button1Style="is-small is-danger button is-rounded my-class"></CoolButton>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <Signup />
+    </div>
+  );
+  /*  <div>
+      <Navbar />
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+      <FormField
+        label="Email"
+        type="email"
+        placeholder="e.g. alexsmith@gmail.com"
+      />
+      <CoolButton
+        button1Style="is-small is-danger button is-rounded my-class"
+        button2Style="is-small is-success button is-rounded my-class"
+      ></CoolButton>
+    </div> */
+};
+
+export default App;
+//App.jsをエクスポート
