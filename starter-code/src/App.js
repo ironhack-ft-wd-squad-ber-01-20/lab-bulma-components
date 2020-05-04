503625f10e7a161cdeeedc24693e4a9038541ea9
Alba-Tros <samipcpoolberlin@yahoo.fr>
2020-05-04 16:51:14 +0200

    Iteration 2 Done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
 import Navbar from "../components/Navbar";
+import FormField from "../components/FormField";
 
 const App = () => {
-    return <Navbar />;
+    return (
+        <div className="container">
+            <Navbar />
+            <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+            <FormField
+                label="Email"
+                type="email"
+                placeholder="e.g. alexsmith@gmail.com"
+            />
+        </div>
+    );
 };
 
 export default App;
