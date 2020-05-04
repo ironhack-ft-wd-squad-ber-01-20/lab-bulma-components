f92bb7a158ba2698e1a96a7cf800926ae8cfae07
robertbozsik <robertbozsik@gmail.com>
2020-05-04 18:30:19 +0200

    some tasks are done

---
+++ starter-code/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "./Navbar.js";
+import Formfield from "./Formfield.js";
+import CoolButton from "./CoolButton.js";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Formfield />
+      <Formfield />
+      <Formfield />
+      <CoolButton />
+    </div>
+  );
+};
+
+export default App;
