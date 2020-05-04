8e1d75feff296ac5ddb4c823a35fece5cc8d787e
Karl Secen <karl.secen@gmail.com>
2020-05-04 18:17:45 +0200

    iteration 1&2

---
+++ starter-code/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "./Navbar";
+import FormField from "./formfield/FormField"
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+      <FormField
+        label="Email"
+        type="email"
+        placeholder="e.g. alexsmith@gmail.com"
+      />
+    </div>
+  );
+};
+
+export default App;
---
+++ starter-code/src/formfield/FormField.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const FormField = (props) => {
+  return (
+    <div className="field" style={{ width: "50%" }}>
+      <label className="label">{props.label}</label>
+      <div className="control">
+        <input className="input" type={props.type} placeholder={props.placeholder} />
+      </div>
+    </div>
+  );
+};
+
+export default FormField;
