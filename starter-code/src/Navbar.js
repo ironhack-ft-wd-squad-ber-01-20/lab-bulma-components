9a67c806cec08af6002f3343bd636c34cdacf73f
jesuscabrera <chuscabrera@gmail.com>
2020-05-04 18:16:33 +0200

    done

---
+++ starter-code/src/Navbar.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+import CoolButton from "./CoolButton";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav class="navbar is-light">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="https://bulma.io">
+            <img
+              src="https://bulma.io/images/bulma-logo.png"
+              alt="Bulma: a modern CSS framework based on Flexbox"
+              width="112"
+              height="28"
+            />
+          </a>
+          <div
+            class="navbar-burger burger"
+            data-target="navbarExampleTransparentExample"
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+
+        <div id="navbarExampleTransparentExample" class="navbar-menu">
+          <div class="navbar-start">
+            <a class="navbar-item" href="/">
+              Home
+            </a>
+          </div>
+
+          <div class="navbar-end">
+            <div class="navbar-item">
+              <div class="field is-grouped">
+                <p class="control">
+                  <CoolButton
+                    className="button is-rounded my-class is-small is-danger"
+                    text="Login"
+                  />
+                </p>
+                <p class="control">
+                  <CoolButton
+                    text="Signup"
+                    className="button is-small is-success"
+                  />
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
