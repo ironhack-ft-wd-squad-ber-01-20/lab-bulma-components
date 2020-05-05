65dac97be47fd46c417fb8be1d14b33992f35c9f
Nate <mcfwesh@gmail.com>
2020-05-05 11:58:00 +0200

    done

---
+++ starter-code/src/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import Signup from "./Signup.jsx";
+
+const App = () => {
+  return <Signup />;
+};
+
+export default App;
---
+++ starter-code/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.js";
+import "bulma/css/bulma.css";
+import Navbar from "./Navbar.jsx";
+
+ReactDOM.render(<App />, document.getElementById("root"));
