d74b1b15146bcdc538e062fc375352c5637eb17d
Benoit Hage <hage.benoit@gmail.com>
2020-07-14 15:51:01 +0200

    finally

---
+++ starter-code/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./Navbar";
+import FormField from "./FormField";
+import Signup from "./SignUp";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Signup />
+    </div>
+  );
+};
+
+export default App;
